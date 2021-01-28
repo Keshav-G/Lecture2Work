80a8d2925098544b5334b801429cdf53f6858379
Keshav <kg249@duke.edu>
2021-01-28 15:55:22 -0500

    Create simple text interface

---
+++ blood_tests.py
@@ -0,0 +1,10 @@
+def interface():
+	while True:
+		print("/nBlood Test Analysis")
+		print("Options")
+		print("9 - Quit")
+		choice = input("Enter an option: ")
+		if choice == "9":
+			return
+
+interface()
