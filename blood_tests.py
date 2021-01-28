9f8f4f6f876eb1eecc4a37199fd728ada7b58a32
Keshav <kg249@duke.edu>
2021-01-28 16:23:43 -0500

    Fixed syntax error in output function. Added option to input to call HDL driver

--- blood_tests.py
+++ blood_tests.py
@@ -6,6 +6,9 @@ def interface():
 		choice = input("Enter an option: ")
 		if choice == "9":
 			return
+		if choice == "1":
+			print("Running HDL Driver")
+			HDL_driver()
 def input_HDL_data():
 	HDL_data = input("Please enter your HDL result: ")
 	return int(HDL_data)
@@ -13,12 +16,12 @@ def input_HDL_data():
 def analyze_HDL_data(result):
 	if result >= 60:
 		return "Normal"
-	elif result >= 40 and result < 60:
+	elif result 40 <= result < 60:
 		return "Borderline low"
 	else:
 		return "Low"
 
-def output_HDL_data(result)
+def output_HDL_data(result):
 	print("Your HDL status is: " + result)
 
 def HDL_driver():
@@ -32,4 +35,3 @@ def HDL_driver():
 	output_HDL_data(status)
 
 interface()
-HDL_driver()
