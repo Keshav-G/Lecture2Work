83bf71613c252377778710a470b36516bf14ca39
Keshav <kg249@duke.edu>
2021-01-28 16:13:29 -0500

    Created HDL analysis driver function and user input function

--- blood_tests.py
+++ blood_tests.py
@@ -6,5 +6,18 @@ def interface():
 		choice = input("Enter an option: ")
 		if choice == "9":
 			return
+def input_HDL_data():
+	HDL_data = input("Please enter your HDL result: ")
+	return int(HDL_data)
+
+def HDL_driver():
+	# Get Data
+	input_HDL_data()
+	
+	# Analyze Data
+	analyze_HDL_data()	
+ 
+	# Output Data
+	output_HDL_data
 
 interface()
